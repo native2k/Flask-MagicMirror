fca99f193b2c2e0c0c312ab15be5514d652f431e
Valentino Constantinou <vc1492a@gmail.com>
2016-11-06 16:10:51 -0600

    updates

--- static/js/styles.js
+++ static/js/styles.js
@@ -1,12 +1,22 @@
-function getTime(adj) {
+// http://stackoverflow.com/a/2998874/5441252
+function zeroPad(num, places) {
+  var zero = places - num.toString().length + 1;
+  return Array(+(zero > 0 && zero)).join("0") + num;
+}
+
+function getTime(adjHour, adjMin) {
     var d = new Date();
-    var n = d.getHours() - adj;
-    var m = d.getMinutes() - adj;
+    var n = d.getHours() - adjHour;
+    var m = zeroPad(d.getMinutes(), 2);
     var time = (String(n) + ':' + String(m));
     document.getElementById('updatetime').innerHTML = time;
     console.log(time);
 }
 
 setInterval(function() {
-    getTime(5);
+    getTime(5, 0);
 }, 5000);
+
+setInterval(function() {
+    window.location.reload(false);
+}, 3600000);
